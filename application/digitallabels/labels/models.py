53384c9b71f249150c510d1f35f4f6534b499344
rbarrettsmall <r.barrettsmall@vam.ac.uk>
2012-03-28 17:42:02 +0100

    Laying out model structure for digital labels backend #3

--- application/digitallabels/labels/models.py
+++ application/digitallabels/labels/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 # Create your models here.
 
-class Label(models.Model):
+class DigitalLabel(models.Model):
     """
     A label describing an individual object
     """
@@ -11,5 +11,22 @@ class Label(models.Model):
     object_number = models.CharField(max_length=16, null=False, blank=False)
     credit_line = models.CharField(max_length=255, null=False, blank=True)
     main_text = models.TextField(blank=True)
-    redownload = models.BooleanField(help_text="WARNING: This may replace your exising content")
+    redownload = models.BooleanField(help_text="""WARNING: This may
+                                         replace your existing content""")
 
+class CMSLabel(models.Model):
+
+    date = models.CharField(max_length=255, null=False)
+    text = models.TextField()
+    digitallabel = models.ForeignKey(DigitalLabel)
+
+class Image(models.Model):
+
+    caption = models.CharField(max_length=255, null=False)
+    digitallabel = models.ForeignKey(DigitalLabel)
+    image_file = models.ImageField(upload_to="labels/images")
+
+class Group(models.Model):
+
+    name = models.CharField(max_length=255, null=False)
+    digitallabels = models.ManyToManyField(DigitalLabel)
