76baa812664ec6db2dfd4163a1f12e52ba4fd863
methodog <sittingbottom@gmail.com>
2012-07-03 16:24:42 +0100

    Closes #12: scroll to specified object in digital label

--- application/labels/views.py
+++ application/labels/views.py
@@ -4,12 +4,13 @@ from django.template import loader, RequestContext
 from labels.models import DigitalLabel
 
 
-def digitallabel(request, digitallabel_id):
-
+def digitallabel(request, digitallabel_id, id=None, pos=None):
     dl = DigitalLabel.objects.get(id=digitallabel_id)
     mobjects = dl.museumobjects.all()
+    if pos is not None:
+        pos = int(pos)
     t = loader.get_template('digitallabel.html')
-    c = RequestContext(request, {'mobjects': mobjects})
+    c = RequestContext(request, {'mobjects':mobjects, 'id':id, 'pos':pos})
     return HttpResponse(t.render(c))
 
 
@@ -17,7 +18,7 @@ def index(request):
     """Lists available digital labels"""
     labels = DigitalLabel.objects.all()
     t = loader.get_template('base.html')
-    c = RequestContext(request, {'labels': labels})
+    c = RequestContext(request, {'labels':labels})
     return HttpResponse(t.render(c))
 
 
