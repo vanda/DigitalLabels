f5e31583dc89b09e4cefa6cd79aacd39125d75cd
richbs <rickbs@gmail.com>
2012-07-06 17:53:26 +0100

    Integrating Tiny MCE WYSIWYG in labels admin.

--- application/labels/templatetags/replace.py
+++ application/labels/templatetags/replace.py
@@ -1,11 +1,12 @@
-import re 
+import re
 
 from django import template
 register = template.Library()
 
+
 @register.filter
-def replace ( string, args ): 
-    search  = args.split(args[0])[1]
+def replace(string, args):
+    search = args.split(args[0])[1]
     replace = args.split(args[0])[2]
 
-    return re.sub( search, replace, string )
+    return re.sub(search, replace, string)
