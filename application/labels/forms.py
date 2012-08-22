6c86be91d0edb824cac9efafffd856aee9208654
richbs <rickbs@gmail.com>
2012-08-22 17:38:12 +0100

    Merging in Santino's fine work on the Portals for #21 and #16

---
+++ application/labels/forms.py
@@ -0,0 +1,30 @@
+from django.forms import ModelForm
+from django.forms.util import flatatt
+from django.forms.widgets import TextInput
+from django.utils.encoding import force_unicode
+from django.utils.safestring import mark_safe
+from labels.models import MuseumObject
+
+class ObjectNumberInput(TextInput):
+    """
+    Class for managing the object_number input editability 
+    """
+    input_type = None # Subclasses must define this.
+
+    def render(self, name, value, attrs=None):
+        if value is None:
+            value = ''
+        final_attrs = self.build_attrs(attrs, type=self.input_type, name=name)
+        if value != '':
+            # Only add the 'value' attribute if a value is non-empty.
+            final_attrs['value'] = force_unicode(self._format_value(value))
+            final_attrs['readonly'] = 'readonly'
+            final_attrs['style'] = 'width: 272px;'
+        return mark_safe(u'<input%s />' % flatatt(final_attrs))
+
+class EditMuseumObjectForm(ModelForm):
+    class Meta:
+        model = MuseumObject
+        widgets = {
+            'object_number': ObjectNumberInput,
+        }
--- application/labels/urls.py
+++ application/labels/urls.py
@@ -2,11 +2,21 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('',
     url(r'^$', 'labels.views.index', name='index'),
+    url(r'^template/$', 'labels.views.template', name='template'),
+    #DigitalLabel urls
     url(r'^digitallabel/(?P<digitallabel_id>\d+)/$',
                             'labels.views.digitallabel', name='digitallabel'),
-    url(r'^digitallabel/(?P<digitallabel_id>\d+)/id/(?P<id>\d+)/$',
+    url(r'^digitallabel/(?P<digitallabel_id>\d+)/objectid/(?P<objectid>\d+)/$',
                             'labels.views.digitallabel', name='digitallabel'),
     url(r'^digitallabel/(?P<digitallabel_id>\d+)/pos/(?P<pos>\d+)/$',
                             'labels.views.digitallabel', name='digitallabel'),
-    url(r'^template/$', 'labels.views.template', name='template'),
+    #Portal urls
+    url(r'^portal/(?P<portal_id>\d+)/$',
+                            'labels.views.portal', name='portal'),
+    url(r'^portal/(?P<portal_id>\d+)/labelid/(?P<labelid>\d+)/$',
+                            'labels.views.portal', name='portal'),
+    url(r'^portal/(?P<portal_id>\d+)/objectid/(?P<objectid>\d+)/$',
+                            'labels.views.portal', name='portal'),
+    url(r'^portal/(?P<portal_id>\d+)/pos/(?P<pos>\d+)/$',
+                            'labels.views.portal', name='portal'),
 )
