93183c3a17a128542a5fe0cdad0b6d670d86a4b6
methodog <sittingbottom@gmail.com>
2012-08-23 14:29:40 +0100

    As part of #25, make Timeout/Kiosk mode dependant on DEBUG setting

--- application/labelsite/settings.py
+++ application/labelsite/settings.py
@@ -135,6 +135,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
 )
 
+INTERNAL_IPS = ('127.0.0.1',)
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
