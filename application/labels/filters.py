333e4cba9d4678aefb1e4736b31c22dce68132e7
Santino Puleio <santinopuleio@gmail.com>
2012-08-29 15:25:28 +0100

    fixing the to highlight the selected portal in the dropdown list

--- application/labels/filters.py
+++ application/labels/filters.py
@@ -1,18 +1,20 @@
 from django.contrib.admin import SimpleListFilter
 from django.db.models import Q
+from django.utils.translation import ugettext_lazy as _
 from labels.models import Portal
 
 class PortalListFilter(SimpleListFilter):
     title = 'portal'
     parameter_name = 'portal__id__exact'
     def lookups(self, request, model_admin):
-        return Portal.objects.all().values_list('id', 'name')
+        qs = Portal.objects.all().values_list('id', 'name')
+        return [(str(x), y) for x, y in qs]
 
     def queryset(self, request, queryset):
         if self.value() is not None:
             return queryset.filter(
-                                   Q(museumobject__portal__exact=self.value()) |
-                                   Q(textlabel__portal__exact=self.value())
-                                   )
+                                   Q(museumobject__portal__id__exact=self.value()) |
+                                   Q(textlabel__portal__id__exact=self.value())
+                                  )
         else:
             return queryset
