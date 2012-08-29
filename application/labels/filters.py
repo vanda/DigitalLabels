10b960c04e6d474a33b80f3243106d21a734e3fa
Santino Puleio <santinopuleio@gmail.com>
2012-08-29 16:34:27 +0100

    adding a "none" option in the drop_down_list of the PortalListFilter to exclude all the images used in both museumobjects and/or textlabels linked to a portal

--- application/labels/filters.py
+++ application/labels/filters.py
@@ -8,13 +8,21 @@ class PortalListFilter(SimpleListFilter):
     parameter_name = 'portal__id__exact'
     def lookups(self, request, model_admin):
         qs = Portal.objects.all().values_list('id', 'name')
-        return [(str(x), y) for x, y in qs]
+        drop_down_list = [(str(x), y) for x, y in qs]
+        drop_down_list.append(('null', _('(None)')))
+        return drop_down_list
 
     def queryset(self, request, queryset):
         if self.value() is not None:
-            return queryset.filter(
-                                   Q(museumobject__portal__id__exact=self.value()) |
-                                   Q(textlabel__portal__id__exact=self.value())
-                                  )
+            if self.value() == 'null':
+                return queryset.exclude(
+                                        Q(museumobject__portal__isnull=False) |
+                                        Q(textlabel__portal__isnull=False)
+                                       )
+            else:
+                return queryset.filter(
+                                       Q(museumobject__portal__id__exact=self.value()) |
+                                       Q(textlabel__portal__id__exact=self.value())
+                                      )
         else:
             return queryset
