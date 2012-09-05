ee3c4cae2f1ce29db4b81c61c41fbab24877e9b8
Santino Puleio <santinopuleio@gmail.com>
2012-09-05 11:49:22 +0100

    creating a custom MuseumObjectForm, to be used with the MuseumObjectAdmin, that exclude the following inputs: gateway_object, dl_position, pt_position; so that the user is forced to use the Jquery sorting tool and multiple radio button to change the excluded fields values

--- labels/forms.py
+++ labels/forms.py
@@ -28,3 +28,11 @@ class EditMuseumObjectForm(ModelForm):
         widgets = {
             'object_number' : ObjectNumberInput,
         }
+
+class MuseumObjectForm(ModelForm):
+    class Meta:
+        model = MuseumObject
+        exclude = ('gateway_object', 'dl_position', 'pt_position')
+        widgets = {
+            'object_number' : ObjectNumberInput,
+        }
