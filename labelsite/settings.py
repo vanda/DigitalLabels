9d8d02c509ca7787202591edcbf1353be60f3163
richbs <rickbs@gmail.com>
2012-11-16 16:13:56 +0000

    Tweaking installed app order to test stability on Cooper.

--- labelsite/settings.py
+++ labelsite/settings.py
@@ -129,13 +129,13 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'labels',
     'grappelli',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'reversion',
-    'labels',
     'sorl.thumbnail',
 )
 
