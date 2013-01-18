c3cb9fda4d2ef31eae9b1e3e67ebd15d2bd6b2da
richbs <rickbs@gmail.com>
2013-01-18 15:31:20 +0000

    De-V&Aing the templates as part of #38.

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import find_packages
 setup(
     name='django-digitallabels',
     version='0.49',
-    author=u'V&A Web Team',
+    author=u'V&A Digital Media',
     author_email='webmaster@vam.ac.uk',
     packages=find_packages(),
     include_package_data=True,
     url='https://github.com/vanda/DigitalLabels',
-    license='BSD licence, see LICENCE.txt',
+    license='BSD licence, see LICENCE',
     description='Provides backend and interface for interactive museum labels',
     long_description=open('README.md').read(),
 )
